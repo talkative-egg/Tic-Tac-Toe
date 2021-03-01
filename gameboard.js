df8bf1e2aa4b1840c3c3cfce1e443cb4983523db
Peter Chen <chenjiajun0825@gmail.com>
2021-03-01 09:45:41 -0500

    Initial commit with game board

---
+++ gameboard.js
@@ -0,0 +1,34 @@
+const gameBoard = (() => {
+
+    let gameboard = ["", "", "", "", "", "", "", "", ""];
+
+    const checkSpot = function(spot){
+        return gameboard[spot] !== "";
+    }
+
+    const setSpot = function(spot, symbol){
+        gameboard[spot] = symbol;
+    }
+
+    const gameEnd = function(){
+
+        for(let i = 0; i < gameboard.length; i += 3){
+            if(gameboard[i] !== "" && gameboard[i] == gameboard[i+1] && gameboard[i] == gameboard[i+2]){
+                return true;
+            }
+        }
+
+        for(let i = 0; i < 3; i++){
+            if(gameboard[i] !== "" && gameboard[i] == gameboard[i+3] && gameboard[i] == gameboard[i+6]){
+                return true;
+            }
+        }
+
+        return (gameboard[0] != "" && gameboard[0] == gameboard[4] && gameboard[0] == gameboard[8])
+            || (gameboard[2] != "" && gameboard[2] == gameboard[4] && gameboard[2] == gameboard[6]);
+
+    }
+
+    return {checkSpot, setSpot, gameEnd};
+
+})();
