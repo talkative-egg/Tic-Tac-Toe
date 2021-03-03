11507307b081cd74bfad00c7d03d5583b6f0af06
Peter Chen <chenjiajun0825@gmail.com>
2021-03-03 13:02:50 -0500

    Improved layout for the grid

--- displayController.js
+++ displayController.js
@@ -3,11 +3,22 @@ const displayController = (() => {
     const gameContainer = document.querySelector(".game-container");
 
     const render = function(arr){
+
+        _removeAllChild(gameContainer);
         
         for(let i = 0; i < arr.length; i++){
             let boardPiece = document.createElement("div");
+            boardPiece.className = "board-piece";
             boardPiece.textContent = arr[i];
             gameContainer.appendChild(boardPiece);
+        }
+
+    }
+
+    const _removeAllChild = function(parent){
+
+        while(parent.firstChild){
+            parent.removeChild(parent.firstChild);
         }
 
     }
